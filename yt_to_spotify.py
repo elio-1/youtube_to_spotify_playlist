b9b4c3c09a0165aa08fa66b7895b23b05a817654
Elio Raphael <69924027+elio75006@users.noreply.github.com>
2022-11-09 11:48:44 +0100

    get title of every video from a youtube url

--- yt_to_spotify.py
+++ yt_to_spotify.py
@@ -1,13 +1,24 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import env
+import googleapiclient.discovery
+import re
+
 
 
 class Sp:
     scope = "playlist-modify-public"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=env.Env.spotipy_client_id, client_secret=env.Env.spotipy_client_secret, redirect_uri="https://localhost:8888/callback",scope=scope
-        ))
+    try:
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    except spotipy.oauth2.SpotifyOauthError:
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=env.Env.spotipy_client_id,
+                client_secret=env.Env.spotipy_client_secret,
+                redirect_uri="https://localhost:8888/callback",
+                scope=scope,
+            )
+        )
 
     @classmethod
     def create_playlist(cls, name):
@@ -23,8 +34,8 @@ class Sp:
 
     @classmethod
     def search_song(cls, song):
-        """ Search for a song. Return the first value matching the artist name of track name """
-        return cls.sp.search(q=song, type="track", limit=1)['tracks']['items'][0]['id']
+        """Search for a song. Return the first value matching the artist name of track name"""
+        return cls.sp.search(q=song, type="track", limit=1)["tracks"]["items"][0]["id"]
 
     def playlist_id(cls):
         return cls.sp.current_user_playlists()["items"][0]["id"]
@@ -34,30 +45,48 @@ class Sp:
 
 
 def main():
-    search_list = manual_input_search()     
-    Sp.create_playlist("test playlist")
+    search_list = list_of_playlist(user_playlist_link())
+    Sp.create_playlist(ask_user_playlist_name())
     tracks = []
     for song in search_list:
         tracks.append(Sp.search_song(song))
     Sp.add_songs(tracks)
 
 
-def manual_input_search():
-    search_list=[]
-    while True:
-        user_input = str(input('Search for songs: '))
-        if user_input == '-stop':
-            break
-        search_list.append(user_input)
-        esign = '=' * 8
-        blank = ' ' * 4
-        print('\n' + esign + blank + "Song added to the list" + blank + esign)
-        print(esign + blank + 'Current list:' + blank + esign + '\n')
-        for item in search_list:
-            print(item)
-        print('\ntype "-stop" to end the list\n')
-    return search_list
+def user_playlist_link():
+    url = input('Youtube Playlist link:')
+    match = re.search(
+    # r"(?:http:|https:)*?\/\/(?:www\.|)(?:youtube\.com|m\.youtube\.com|youtu\.|youtube-nocookie\.com).*(?:v=|v%3D|v\/|(?:a|p)\/(?:a|u)\/\d.*\/|watch\?|vi(?:=|\/)|\/embed\/|oembed\?|be\/|e\/)([^&?%#\/\n]*)", # credit: https://github.com/rodrigoborgesdeoliveira
+    r"(?:http:|https:)*?\/\/(?:www\.|)(?:youtube\.com|m\.youtube\.com|youtu\.|youtube-nocookie\.com).*(?:list=|v%3D|v\/|(?:a|p)\/(?:a|u)\/\d.*\/|list\?|vi(?:=|\/)|\/embed\/|oembed\?|be\/|e\/)([^&?%#\/\n]*)",
+    url,
+    )
+    return match.group(1)
 
+
+def ask_user_playlist_name():
+    return input('Playlist name: ')
+
+
+def list_of_playlist(playlist_id):
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = env.Env.youtube_api_key
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
+    )
+
+    request = youtube.playlistItems().list(
+        part="snippet,contentDetails", 
+        maxResults=200,
+        playlistId=playlist_id
+    )
+    response = request.execute()
+    playlist_songs = []
+    for item in response["items"]:
+        if item["snippet"]['title'] != 'Deleted video':
+            playlist_songs.append(item["snippet"]['title'])
+    return playlist_songs 
 
 if __name__ == "__main__":
     main()
